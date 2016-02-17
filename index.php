f078eb1fef15ad9a2e5280e3a5d0d0145157e3c7
Rick van der Burg <rick@pixcero.nl>
2016-02-17 17:00:36 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,114 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Rick van der Burg - portfolio</title>
+    <link rel="stylesheet" href="assets/css/template.css">
+    <link rel="stylesheet" href="assets/plugins/font-awesome/css/font-awesome.min.css">
+</head>
+<body>
+    <header role="banner">
+        <nav role="navigation">
+            <div class="container">
+                <div class="logo">
+                    <a href="#">
+                        <h2>Rick van der Burg</h2>
+                    </a>
+                </div>
+                <div class="navbar-inner">
+                    <ul role="menu">
+                        <li role="menuitem">
+                            <a href="#">Home</a>
+                        </li>
+                        <li role="menuitem">
+                            <a href="#about">Over mij</a>
+                        </li>
+                        <li role="menuitem">
+                            <a href="#portfolio">Portfolio</a>
+                        </li>
+                        <li role="menuitem">
+                            <a href="#work">Werk</a>
+                        </li>
+                        <li role="menuitem">
+                            <a href="#contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="container">
+            <div class="slogan">
+                <h1>"Well begun is half done"</h1>
+            </div>
+        </div>
+        <a href="#" class="nextSection">
+            <i class="fa fa-angle-down"></i>
+        </a>
+    </header>
+    <section id="about" class="page">
+        <div class="container">
+            <div class="title underline">
+                <h2>Over mij</h2>
+            </div>
+        </div>
+    </section>
+    <section class="page bg-grey" id="portfolio">
+        <div class="container">
+            <div class="title underline">
+                <h2>Portfolio</h2>
+            </div>
+        </div>
+    </section>
+    <section class="page" id="work">
+        <div class="container">
+            <div class="title underline">
+                <h2>Werk</h2>
+            </div>
+        </div>
+    </section>
+    <section class="page bg-grey" id="contact">
+        <div class="container">
+            <div class="title underline">
+                <h2>Contact</h2>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="container">
+            <div class="logo center">
+                <a href="#">
+                    <h2>Rick van der Burg</h2>
+                </a>
+            </div>
+            <div class="copyright">
+                &copy; Copyright <?= date('Y') ?>. All Rights Reserved.
+            </div>
+            <div class="social-icons">
+                <ul>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-github"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-facebook"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-twitter"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-linkedin"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
