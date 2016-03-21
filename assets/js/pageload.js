5165f1a1290bc204af53e0184ff03fac9f7b531c
Rick van der Burg <rick@pixcero.nl>
2016-03-21 13:19:12 +0100

    first version of the website

--- assets/js/pageload.js
+++ assets/js/pageload.js
@@ -1,5 +1,12 @@
 $(function() {
     scrollDown();
+    changeNavWhileScrolling();
+    portfolio();
+    addScrollClass();
+    parallax();
+    modals();
+    useEscInModals();
+    worksWith();
 });
 
 // Scroll down to certain element on page
@@ -11,4 +18,98 @@ var scrollDown = function() {
             scrollTop: $(element).offset().top
         }, 1000);
     });
+}
+
+// Add class 'scroll' to navigation when scrolling
+var changeNavWhileScrolling = function() {
+    $(window).on('scroll', function() {
+        addScrollClass();
+    });
+}
+
+var addScrollClass = function() {
+    var scrollHeight = $(this).scrollTop();
+    if(scrollHeight > 10) {
+        $("nav").addClass('scroll');
+    } else {
+        $("nav").removeClass('scroll');
+    }
+}
+
+// Add OwlCarousel plugin to portfolio section
+var portfolio = function() {
+    $("#owl-portfolio").owlCarousel({
+        items: 1,
+        singleItem: true
+    });
+}
+
+var parallax = function() {
+    $(document).ready(function() {
+        if ($("#js-parallax-window").length) {
+            parallax();
+        }
+    });
+
+    $(window).scroll(function(e) {
+        if ($("#js-parallax-window").length) {
+            parallax();
+        }
+    });
+
+    function parallax(){
+        if( $("#js-parallax-window").length > 0 ) {
+            var plxBackground = $("#js-parallax-background");
+            var plxWindow = $("#js-parallax-window");
+
+            var plxWindowTopToPageTop = $(plxWindow).offset().top;
+            var windowTopToPageTop = $(window).scrollTop();
+            var plxWindowTopToWindowTop = plxWindowTopToPageTop - windowTopToPageTop;
+
+            var plxBackgroundTopToPageTop = $(plxBackground).offset().top;
+            var windowInnerHeight = window.innerHeight;
+            var plxBackgroundTopToWindowTop = plxBackgroundTopToPageTop - windowTopToPageTop;
+            var plxBackgroundTopToWindowBottom = windowInnerHeight - plxBackgroundTopToWindowTop;
+            var plxSpeed = 0.35;
+
+            plxBackground.css('top', - (plxWindowTopToWindowTop * plxSpeed) + 'px');
+        }
+    }
+}
+
+var modals = function() {
+    $(function() {
+        $("#modal-1").on("change", function() {
+            if ($(this).is(":checked")) {
+                $("body").addClass("modal-open");
+            } else {
+                $("body").removeClass("modal-open");
+            }
+        });
+
+        $(".modal-fade-screen, .modal-close").on("click", function() {
+            closeModals();
+        });
+
+        $(".modal-inner").on("click", function(e) {
+            e.stopPropagation();
+        });
+    });
+}
+
+var closeModals = function() {
+    $(".modal-state:checked").prop("checked", false).change();
+}
+
+// When ESC key is pressed, close Modals
+var useEscInModals = function() {
+    $(document).keyup(function(e) {
+        if (e.keyCode == 27) {
+            closeModals();
+        }
+    });
+}
+
+var worksWith = function() {
+    $(".owl-carousel").owlCarousel();
 }
--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <title>Rick van der Burg - portfolio</title>
     <link rel="stylesheet" href="assets/css/template.css">
+    <link rel="stylesheet" href="assets/plugins/owl-carousel/owl.carousel.css">
+    <link rel="stylesheet" href="assets/plugins/owl-carousel/owl.theme.css">
     <link rel="stylesheet" href="assets/plugins/font-awesome/css/font-awesome.min.css">
 </head>
 <body>
@@ -32,10 +34,14 @@
                             <a href="#portfolio">Portfolio</a>
                         </li>
                         <li role="menuitem">
-                            <a href="#work">Werk</a>
+                            <a href="http://github.com/itsrd" class="icon">
+                                <i class="fa fa-github"></i>
+                            </a>
                         </li>
                         <li role="menuitem">
-                            <a href="#contact">Contact</a>
+                            <a href="https://nl.linkedin.com/in/rickvdburg" class="icon">
+                                <i class="fa fa-linkedin"></i>
+                            </a>
                         </li>
                     </ul>
                 </div>
@@ -59,6 +65,11 @@
             <div class="title underline">
                 <h2>Over mij</h2>
             </div>
+            <div class="content">
+                <div class="text-center text-block">
+                    Mijn naam is Rick van der Burg en ik studeer momenteel MediaTechnologie op Hogeschool Rotterdam. Naast studeren heb ik momenteel mijn eigen bedrijfje opgezet met als doel zoveel mogelijk te leren naast mijn schoolwerkzaamheden.  Voordat ik aan deze studie ben begonnen heb ik de studie MediaTechnologie gedaan op het Grafisch Lyceum Rotterdam, hierdoor heb ik veel ervaring op kunnen doen met verschillende programmeertalen en ben ik erachter gekomen dat backend development echt mijn ding is.
+                </div>
+            </div>
         </div>
     </section>
     <!-- / About section -->
@@ -69,25 +80,64 @@
             <div class="title underline">
                 <h2>Portfolio</h2>
             </div>
+            <div class="content portfolio">
+                <div class="col-4">
+                    <div class="item">
+                        <img src="assets/img/portfolio/schoonheidssalonmandy-small.png" alt="Schoonheidssalon Mandy">
+                        <div class="moreInfo">
+                            <label for="schoonheidssalonMandy">
+                                Meer informatie
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="item">
+                        <img src="assets/img/portfolio/4staying-small.png" alt="4Staying">
+                        <div class="moreInfo">
+                            <label for="4staying">
+                                Meer informatie
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="item">
+                        <img src="assets/img/portfolio/playplastic-small.png" alt="PlayPlastic">
+                        <div class="moreInfo">
+                            <label for="playplastic">
+                                Meer informatie
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
     <!-- / Portfolio section -->
 
-    <!-- Work section -->
-    <section class="page" id="work">
-        <div class="container">
-            <div class="title underline">
-                <h2>Portfolio</h2>
-            </div>
+    <div id="js-parallax-window" class="parallax-window">
+        <div class="parallax-static-content">
         </div>
-    </section>
-    <!-- / Work section -->
+        <div id="js-parallax-background" class="parallax-background"></div>
+    </div>
 
     <!-- Contact section -->
-    <section class="page bg-grey" id="contact">
+    <section class="page">
         <div class="container">
-            <div class="title underline">
-                <h2>Contact</h2>
+            <div class="content">
+                <div class="owl-carousel">
+                    <div><img src="assets/img/skills/git.png" alt="Git"> </div>
+                    <div><img src="assets/img/skills/github.png" alt="github"> </div>
+                    <div><img src="assets/img/skills/laravel.png" alt="Laravel"> </div>
+                    <div><img src="assets/img/skills/wordpress.png" alt="Wordpress"> </div>
+                    <div><img src="assets/img/skills/ubuntu.png" alt="Ubuntu"> </div>
+                    <div><img src="assets/img/skills/phpstorm.png" alt="PHPStorm"> </div>
+                    <div><img src="assets/img/skills/slack.png" alt="Slack"> </div>
+                    <div><img src="assets/img/skills/asana.png" alt="Asana"> </div>
+                    <div><img src="assets/img/skills/koffie.png" alt="Koffie"> </div>
+                    <div><img src="assets/img/skills/sass.png" alt="Sass"> </div>
+                </div>
             </div>
         </div>
     </section>
@@ -107,22 +157,17 @@
             <div class="social-icons">
                 <ul>
                     <li>
-                        <a href="#">
+                        <a href="http://github.com/itsrd">
                             <i class="fa fa-github"></i>
                         </a>
                     </li>
                     <li>
-                        <a href="#">
-                            <i class="fa fa-facebook"></i>
+                        <a href="">
+                            <i class="fa fa-envelope-o"></i>
                         </a>
                     </li>
                     <li>
-                        <a href="#">
-                            <i class="fa fa-twitter"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
+                        <a href="https://linkedin.com/in/rickvdburg">
                             <i class="fa fa-linkedin"></i>
                         </a>
                     </li>
@@ -132,8 +177,61 @@
     </footer>
     <!-- / Contact section -->
 
+    <!-- Modals -->
+    <div class="modal">
+        <input class="modal-state" id="schoonheidssalonMandy" type="checkbox" />
+        <div class="modal-fade-screen">
+            <div class="modal-inner">
+                <div class="modal-close" for="modal-1"></div>
+                <h1>Schoonheidssalon Mandy</h1>
+                <p class="modal-intro">Schoonheidssalon Mandy is een opdracht die ik gedaan heb voor mijn eigen bedrijf Pixcero. Schoonheidssalon Mandy had hiervoor nog geen website en wilde graag een website hebben die goed beheerbaar was om in de toekomst eventueel teksten aan te passen en zelf acties te kunnen plaatsen wanneer nodig. Ik heb samen met Wesley Cheung (Frontender/Designer) de website gemaakt. Tijdens het bouwen van de website heb ik me vooral bezig gehouden met het maken van het template in Wordpress.</p>
+                <p>
+                    <span class="text-bold">Type Project: </span> Wordpress <br>
+                    <span class="text-bold">In samenwerking met: </span> <a href="http://wmcheung.nl">Wesley Cheung</a>
+                </p>
+
+                <a href="http://schoonheidssalonmandy.nl" target="_blank" class="btn btn-lg"><i class="fa fa-paper-plane"></i> Preview</a>
+            </div>
+        </div>
+    </div>
+    <div class="modal">
+        <input class="modal-state" id="4staying" type="checkbox" />
+        <div class="modal-fade-screen">
+            <div class="modal-inner">
+                <div class="modal-close" for="modal-1"></div>
+                <h1>4Staying</h1>
+                <p class="modal-intro">4Staying is een website waar flexwerkers een huis of appartement kunnen huren voor een bepaalde periode. Daarnaast is het ook mogelijk voor particulieren om hun huis, kamer of appartement te verhuren aan flexwerkers. Dit is het grootste project waar ik aan meegewerkt heb en daardoor heb ik ook enorm veel geleerd om dit soort projecten nog beter aan te kunnen pakken in de toekomst. Samen met 3 andere ZZP'ers heb ik dit project mogen doen.</p>
+                <p>
+                    <span class="text-bold">Type Project: </span> Maatwerk in Laravel<br>
+                    <span class="text-bold">In samenwerking met: </span> Mees Overdevest, Remco Visser en <a href="http://wmcheung.nl">Wesley Cheung</a>
+                </p>
+                <a href="#" target="_blank" class="btn btn-lg"><i class="fa fa-code-fork"></i> In development</a>
+            </div>
+        </div>
+    </div>
+    <div class="modal">
+        <input class="modal-state" id="playplastic" type="checkbox" />
+        <div class="modal-fade-screen">
+            <div class="modal-inner">
+                <div class="modal-close" for="modal-1"></div>
+                <h1>PlayPlastic</h1>
+                <p class="modal-intro">PlayPlastic is een project voor school waarin ik vooral heb gefocust op de backend van het project. Het doel was om plastic in de oceanen aan te pakken door bij de oorzaak te beginnen. Per dag produceren wij een enorme hoeveelheid aan plastic en willen daarom mensen stimuleren om plastic te scheiden van het normale afval. Dit deden we door een speciale prullenbak met een klein spelletje waar je punten mee kon scoren, vervolgens kon je met deze punten bepaalde kortingen kopen.</p>
+                <p>
+                    <span class="text-bold">Type Project: </span> School project (Maatwerk in Laravel)<br>
+                    <span class="text-bold">In samenwerking met: </span> Deborah, Lennart, Ugurkan en <a href="http://wmcheung.nl">Wesley Cheung</a>
+                </p>
+                <a href="http://github.com" target="_blank" class="btn btn-lg"><i class="fa fa-github"></i> Github</a>
+                <a href="http://playplastic.pixcero.nl" target="_blank" class="btn btn-lg"><i class="fa fa-paper-plane"></i> Preview</a>
+            </div>
+        </div>
+    </div>
+
+    <!-- / Modals -->
+
+
     <!-- Scripts -->
     <script src="assets/plugins/jquery/jquery.min.js"></script>
+    <script src="assets/plugins/owl-carousel/owl.carousel.min.js"></script>
     <script src="assets/js/pageload.js"></script>
 </body>
 </html>
